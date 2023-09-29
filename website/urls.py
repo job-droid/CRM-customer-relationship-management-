db9a1548caf2627a8df7dba87de67379848cd62b
Eyob Abebe <eyobabebe143sa@gmail.com>
2023-09-29 13:23:13 +0300

    Finished One

--- website/urls.py
+++ website/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('', views.home, name='home'),
     # path('', views.login_user, name='login'),
     path('', views.logout_user, name='logout'),
-    path('register/', views.register_user, name='register_user')
+    path('register/', views.register_user, name='register_user'),
+    path('customer/<int:pk>', views.customer_record, name='record'),
+    path('delete_record/<int:pk>', views.delete_customer, name="delete_record"),
+    path('add_customer/', views.add_customer, name='add_customer'),
+    path('update_customer/<int:pk>', views.update_customer, name="update_customer")
 ]
--- website/views.py
+++ website/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm, AddCustomer
+from .models import Customer
+
 
 # Create your views here.
 def home(request):
+    customers = Customer.objects.all()
     # Check to see if logging in
     if request.method == 'POST':
         username = request.POST['Username']
@@ -18,7 +22,7 @@ def home(request):
             messages.success(request, "Please Try Again...")
             return redirect('home')
     else:
-        return render(request, 'website/home.html', {})
+        return render(request, 'website/home.html', {"customers":customers})
     
 
 def login_user(request):
@@ -30,4 +34,68 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    return render(request, 'website/register.html', {})
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, 'You Have Successfully Registered!')
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'website/register.html', {'form': form})
+    return render(request, 'website/register.html', {'form': form})
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        customer_record = Customer.objects.get(id=pk)
+        return render(request, 
+                      "website/customer_record.html", 
+                      {"cust_record": customer_record})
+    else:
+        messages.success(request, 'You Must Be Logged In To That Page!')
+        return redirect('home')
+    
+# Delete a customer by thier id
+def delete_customer(request, pk):
+    if request.user.is_authenticated:
+        delete_record = Customer.objects.get(id=pk)
+        delete_record.delete()
+        messages.success(request, "Customer Record Deleted Successfully!")
+        return redirect('home')
+    else:
+        messages.success(request,"You Must Be Logged In To That Page!")
+        return redirect('home')
+    
+    
+# Add a customer
+def add_customer(request):
+    form = AddCustomer(request.POST or None)
+    if  request.user.is_authenticated:
+        if request.method == 'POST':
+            if form.is_valid():
+                add_customer = form.save()
+                messages.success(request, "Customer Add")
+                return redirect('home')
+        return render(request, 'website/add_customer.html',{'form':form})
+    else:
+        messages.success(request,"You Must Be Logged In To That Page!")
+        return redirect('home')
+        
+def update_customer(request, pk):
+    if request.user.is_authenticated:
+        current_customer = Customer.objects.get(id=pk)
+        form = AddCustomer(request.POST or None, instance=current_customer)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"Customer Has Been Updated!")
+            return redirect('home')
+        else:
+            return render(request, 'website/update_customer.html',{'form':form})
+    else:
+        messages.success(request,"You Must Be Logged In To That Page!")
+        return redirect('home')
